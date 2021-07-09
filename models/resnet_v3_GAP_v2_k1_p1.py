307f9ca0bb920e56145da2980338d6a01d2241b2
Muhsin <askmuhsin@gmail.com>
2021-07-09 12:11:02 +0530

    added more model variants with different endings for testing

--- models/resnet_v3_GAP_v2_k1_p1.py
+++ models/resnet_v3_GAP_v2_k1_p1.py
@@ -49,6 +49,7 @@ class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
+        self.num_classes = num_classes
 
         self.conv1 = nn.Conv2d(
             3, 64, kernel_size=3,
@@ -59,7 +60,10 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Conv2d(
+            512, 10, kernel_size=1, stride=1, padding=1, bias=False
+        )
+        self.gap = nn.AvgPool2d(kernel_size=6)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -75,9 +79,11 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
+        
         out = self.linear(out)
+        out = self.gap(out)
+        out = out.view(-1, self.num_classes)
+
         return out
 
 
