5363298d2c557e90bc0f2b8b31633b90a395b009
Muhsin <askmuhsin@gmail.com>
2021-08-08 01:25:16 +0800

    update default input size

--- utils/viz.py
+++ utils/viz.py
@@ -5,7 +5,7 @@ from torchsummary import summary
 import matplotlib.pyplot as plt
 
 
-def show_model_summary(model, input_size=(3, 28, 28)):
+def show_model_summary(model, input_size=(3, 32, 32)):
     summary(model, input_size=input_size)
 
 
